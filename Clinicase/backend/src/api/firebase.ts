288dbae507f28deff00749ea6491a39f0067448f
Regasri <114863057+Regasri@users.noreply.github.com>
2025-11-02 14:28:54 +0530

    Delete Clinicase/backend/src/api/firebase.ts

--- Clinicase/backend/src/api/firebase.ts
+++
@@ -1,9 +0,0 @@
-import admin from "firebase-admin";
-admin.initializeApp({
-  credential: admin.credential.applicationDefault(),
-  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-});
-
-export function uploadToFirebase(file: Buffer, userId: string) {
-  // Implement upload logic
-}
